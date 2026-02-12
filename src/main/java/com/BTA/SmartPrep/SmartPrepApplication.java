2baf9b0908968d322ab267ab72c84edf7cc2467a
jojo <josephbarron1016@gmail.com>
2026-02-12 10:36:36 -0600

    Initial Spring Boot setup

---
+++ src/main/java/com/BTA/SmartPrep/SmartPrepApplication.java
@@ -0,0 +1,13 @@
+package com.BTA.SmartPrep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartPrepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BTA/SmartPrep/SmartPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BTA.SmartPrep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPrepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
